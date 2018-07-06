bb04b424a5444c91084c59807b6237d39c5ea2a9
Jeffry Valadez <jeffry.valadez.94@gmail.com>
2018-07-06 01:23:02 -0500

    sync user input to quereied values from api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,6 +21,8 @@
 -->
 
 var favMovie = prompt("TRIVA TIME! What's your favorite movie?");
+var correct = 0;
+var wrong = 0;
 
 console.log(favMovie);
 
@@ -32,29 +34,38 @@ $.ajax({
 }).then(function(response){
   console.log(response);
 
-var correct = 0;
-var wrong = 0;
+  // question1(response);
+  // question2(response);
+  question3(response);
 
 
-var buttonIDs = ["Option1", "Option2", "Option3","Option4"];
 
-var randomYear = Math.floor(Math.random() * (2018 - 1960) + 1960);
-console.log(randomYear);
 
-var randomID = Math.floor(Math.random() * (4 - 0) + 0);
 
-var movieYear = response.Year;
-console.log(movieYear);
+});
+function question1(response){
 
-for (var i = 0; i < buttonIDs.length; i++){
+  //Changes div to contain question. What year was "movie" relased?
+  var buttonIDs = ["Option1", "Option2", "Option3","Option4"];
+
   var randomYear = Math.floor(Math.random() * (2018 - 1960) + 1960);
   console.log(randomYear);
-  var buttonID = buttonIDs[i];
 
-$('#' + buttonID).val(randomYear);
+  var randomID = Math.floor(Math.random() * (4 - 0) + 0);
+
+  var movieYear = response.Year;
+  console.log(movieYear);
+
+  for (var i = 0; i < buttonIDs.length; i++){
+    var randomYear = Math.floor(Math.random() * (2018 - 1960) + 1960);
+    console.log(randomYear);
+    var buttonID = buttonIDs[i];
+    $("#" + buttonID).val(randomYear);
 
 
-}
+  }
+  var correctButton = $('#' + buttonIDs[randomID]).val(movieYear);
+  var correctAnswer = $(correctButton).val();
 
 
   console.log($('#Option1').val());
@@ -64,8 +75,132 @@ $('#' + buttonID).val(randomYear);
   console.log(randomID);
   console.log($("#" + buttonIDs[randomID]).val(movieYear));
 
-$('.button').click(function(){
-  console.log(this);
-  })
+  $('.button').click(function(){
+    console.log(this);
+    var selectedButton = this;
+    var userAnswer = $(selectedButton).val();
+    console.log("userAnswer:" + userAnswer);
+    console.log("correctAnswer:"+ correctAnswer);
 
-});
+    if (userAnswer === correctAnswer){
+      correct++;
+    }else{
+      wrong++;
+    }
+
+
+
+
+    alert("# of Correct Answers:" + correct + "   # of Wrong Answers:" + wrong);
+  })
+}
+
+
+//------Question 2-----------------
+
+
+function question2(response){
+
+  //changes div text to "what was this film rated?"
+  var buttonIDs = ["Option1", "Option2", "Option3","Option4"];
+
+  var ratings = ["G","PG","PG-13","R"];
+
+  var randomID = Math.floor(Math.random() * (4 - 0) + 0);
+
+  var movieRating = response.Rated;
+  console.log(movieRating);
+
+  for (var i = 0; i < ratings.length; i++){
+    var rating = ratings[i];
+    $("#" + buttonIDs[i]).val(rating);
+
+
+  }
+  var correctButton = $('#' + buttonIDs[randomID]).val(movieRating);
+  var correctAnswer = $(correctButton).val();
+
+
+  console.log($('#Option1').val());
+  console.log($('#Option2').val());
+  console.log($('#Option3').val());
+  console.log($('#Option4').val());
+  console.log(randomID);
+  console.log($("#" + buttonIDs[randomID]).val(movieRating));
+
+  $('.button').click(function(){
+    console.log(this);
+    var selectedButton = this;
+    var userAnswer = $(selectedButton).val();
+    console.log("userAnswer:" + userAnswer);
+    console.log("correctAnswer:"+ correctAnswer);
+
+    if (userAnswer === correctAnswer){
+      correct++;
+    }else{
+      wrong++;
+    }
+
+
+
+
+    alert("# of Correct Answers:" + correct + "   # of Wrong Answers:" + wrong);
+  })
+}
+
+
+//---------------question 3---------
+function question3(response){
+
+  //changes div text to "what was this film rated?"
+  var buttonIDs = ["Option1", "Option2", "Option3","Option4"];
+
+  var randomRunTime = Math.floor(Math.random() * (146 - 87) + 87);
+
+
+  var randomID = Math.floor(Math.random() * (4 - 0) + 0);
+
+  var movieRunTime = response.Runtime;
+  console.log(movieRunTime);
+
+  for (var i = 0; i < buttonIDs.length; i++){
+    var buttonID = buttonIDs[i];
+    $("#" + buttonID).val("" + randomRunTime + " min");
+
+
+  }
+  var correctButton = $('#' + buttonIDs[randomID]).val(movieRunTime);
+  var correctAnswer = $(correctButton).val();
+
+
+  console.log($('#Option1').val());
+  console.log($('#Option2').val());
+  console.log($('#Option3').val());
+  console.log($('#Option4').val());
+  console.log(randomID);
+  console.log($("#" + buttonIDs[randomID]).val(movieRunTime));
+
+  $('.button').click(function(){
+    console.log(this);
+    var selectedButton = this;
+    var userAnswer = $(selectedButton).val();
+    console.log("userAnswer:" + userAnswer);
+    console.log("correctAnswer:"+ correctAnswer);
+
+    if (userAnswer === correctAnswer){
+      correct++;
+    }else{
+      wrong++;
+    }
+
+
+
+
+    alert("# of Correct Answers:" + correct + "   # of Wrong Answers:" + wrong);
+  })
+}
+
+
+
+
+//Creat time tha counts down.. when counts down ends the game by Creating Alert.
